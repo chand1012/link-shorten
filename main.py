abeeda9a547bfe06b57f00ed938d1392d36bbd18
chand1012 <chandler@chand1012.dev>
2021-06-23 11:15:01 -0400

    Fix CORS

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Header
 from starlette.responses import RedirectResponse, Response
-
+from fastapi.middleware.cors import CORSMiddleware 
 from load_cors import load_cors
 from database import get_link, set_link
 from generator import get_unique_str
@@ -22,7 +22,6 @@ config = load_cors(CORS_CONFIG)
 app = FastAPI()
 
 if config:
-    from fastapi.middleware.cors import CORSMiddleware 
     app.add_middleware(CORSMiddleware, **config)
 
 @app.post('/new')
@@ -40,6 +39,10 @@ async def new_link(body: NewLink, x_api_key: Optional[str] = Header(None)):
         return Response(None, 500)
 
     return body
+
+@app.options('/new')
+async def options():
+    return Response('Success', 200, headers={'Access-Control-Allow-Origin': '*'})
 
 @app.get('/')
 async def home():
